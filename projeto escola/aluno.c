599c9af6d5a6ea16b3dbd351a75a895a8e5ace66
Pedro Ivo Dantas <pedroivodantas@gmail.com>
2026-03-28 22:08:01 +0000

    cadastro teste

--- projeto escola/aluno.c
+++ projeto escola/aluno.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <time.h>
 #include <stdbool.h>
+#define TAM 5
 
 typedef struct data{
     int dia;
@@ -13,10 +14,48 @@ typedef struct data{
     char cpf[12];
     char nome[50];
     char sexo;
+    char matricula[11];
     Data nascimento;
 } Dados;
 
 int main(){
+
+    Dados listaAlunos[TAM];
+
+    int qtdAlunos = 0;
+    int sair = 0;
+    int opcao;
+
+    while (sair == 0){
+        printf("--------MENU--------");
+        printf("\nEscolha um opcao:\n");
+        printf("\n0 - Sair\n");
+        printf("\n1 - Cadastrar aluno\n");
+        printf("\n2 - Cadastrar professor\n");
+        printf("\n3 - Cadastrar disciplina\n");
+        printf("\n4 - Pesquisar Listas\n");
+        scanf("%d",&opcao);
+
+        switch (opcao){
+            case 0:{
+                //sair do programa
+                sair = 1;
+                break;
+            }
+            case 1:{
+                //cadastrar aluno
+                if (qtdAlunos == TAM){
+                    printf("Lista de Alunos Completa");
+                } else {
+                    int i;
+                    for(i = 0; i < TAM; i++)
+                        printf("Digite a matricula do aluno:");
+                        fgets(listaAlunos[i].matricula,11,stdin);
+                }
+                break;
+            }
+        }
+    }
 
 
 }
