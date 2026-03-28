699065d4b2dc91876532738ff4a3cda1bd748dd9
p-ivotech <pedroivodantas@gmail.com>
2026-03-28 18:27:30 -0300

    go

---
+++ projeto escola/aluno.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+
+typedef struct data{
+    int dia;
+    int mes;
+    int ano;
+} Data;
+ typedef struct {
+    char cpf[12];
+    char nome[50];
+    char sexo;
+    Data nascimento;
+} Dados;
+
+int main(){
+
+
+}
